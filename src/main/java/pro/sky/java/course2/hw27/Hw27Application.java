2d52fc35251a640bb626e188113e2c90929db198
nohero_ <serega1552@yandex.ru>
2022-05-13 20:06:22 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/hw27/Hw27Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw27Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hw27/Hw27ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
